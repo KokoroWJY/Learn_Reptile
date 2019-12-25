6a0507149b0b99ba34c8d8d736973528c75d175d
KokoroWJY <oumainoli@gmail.com>
2019-12-25 16:48:47 +0800

    爬虫之旅

--- Second/One.py
+++ Second/One.py
@@ -6,10 +6,10 @@ bs = BeautifulSoup(html.read(), 'html.parser')
 nameList = bs.findAll('span', {'class': 'green'})
 for name in nameList:
     print(name.get_text())
-# print(bs.find('img', {'src': '../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
+print(bs.find('img', {'src': '../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
 
-# for sibling in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
-#     print(sibling)
+for sibling in bs.find('table', {'id': 'giftList'}).tr.next_siblings:
+    print(sibling)
 
-# for child in bs.find('table', {'id': 'giftList'}).children:
-#     print(child)
+for child in bs.find('table', {'id': 'giftList'}).children:
+    print(child)
