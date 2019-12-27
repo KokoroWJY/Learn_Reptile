b265cfed7e7f0f716a6fc35923b1cf984d4b82ea
KokoroWJY <oumainoli@gmail.com>
2019-12-27 16:02:53 +0800

    爬虫之旅

---
+++ Third/WiKi.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+proxies = {
+    "http": "http://127.0.0.1:1080",
+    'https': 'http://127.0.0.1:1080'
+}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
+}
+html = requests.get('https://en.wikipedia.org/wiki/Kevin_Bacon', headers=headers, proxies=proxies, timeout=5)
+bs = BeautifulSoup(html.text, 'html.parser')
+print(bs.find_all('a'))
+for link in bs.find_all('a'):
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
+
