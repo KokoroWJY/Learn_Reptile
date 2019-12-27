a6ae67c2b579706d00ec273ef8f50084d8845836
KokoroWJY <oumainoli@gmail.com>
2019-12-27 16:03:39 +0800

    爬虫之旅 "代理设置"

--- Third/WiKi.py
+++ Third/WiKi.py
@@ -8,7 +8,7 @@ proxies = {
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
 }
-html = requests.get('https://en.wikipedia.org/wiki/Kevin_Bacon', headers=headers, proxies=proxies, timeout=5)
+html = requests.get('https://en.wikipedia.org/wiki/Kein_Bacon', headers=headers, proxies=proxies, timeout=5)
 bs = BeautifulSoup(html.text, 'html.parser')
 print(bs.find_all('a'))
 for link in bs.find_all('a'):
