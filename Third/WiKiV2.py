0dea5ce570ee33afa56eea30675618873dadc7a2
KokoroWJY <oumainoli@gmail.com>
2019-12-30 18:09:31 +0800

    爬虫之旅--"代理设置"--"wiki爬取"

--- Third/WiKiV2.py
+++ Third/WiKiV2.py
@@ -10,21 +10,27 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
 }
 
-pages = set()
+pages = set()  # 全局变量集合(已抓取的变量集合)
 
 
 def getLinks(pageUrl):
     global pages
-    html = requests.get('http://en.wikipedia.org{}'.format(pageUrl), headers=headers, proxies=proxies, timeout=5)
+    html = requests.get('http://en.wikipedia.org{}'.format(pageUrl), headers=headers, proxies=proxies, timeout=100)
     bs = BeautifulSoup(html.text, 'html.parser')
-    for link in bs.find_all('a', href=re.compile('^(/wiki)')):
+    try:
+        print(bs.h1.get_text())
+        print(bs.find(id='ca-edit').find('span').find('a').attrs['href'])
+    except AttributeError:
+        print("页面缺少一页, 不用担心!")
+
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
-                # we have encountered a new page 我们会遇到一个新的页面
-                newPage = link.attrs['href']
-                print(newPage)
-                pages.add(newPage)
-                getLinks(newPage)
+                newPages = link.attrs['href']
+                print('-' * 20)
+                print(newPages)
+                pages.add(newPages)
+                getLinks(newPages)
 
 
 getLinks('')
