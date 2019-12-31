e8ba111f076f263104b4bfbd843d2b6e6a87813b
KokoroWJY <oumainoli@gmail.com>
2019-12-31 15:59:59 +0800

    爬虫之旅

---
+++ Third/WiKiV3.py
@@ -0,0 +1,67 @@
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import requests
+import random
+import re
+import datetime
+
+proxies = {
+    "http": "http://127.0.0.1:1080",
+    'https': 'http://127.0.0.1:1080'
+}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
+}
+
+pages = set()
+random.seed(datetime.datetime.now())
+
+
+# 获取页面中所有内链的列表
+def getInternalLinks(bs, includeUrl):
+    includeUrl = '{}://{}'.format(urlparse(includeUrl).scheme, urlparse(includeUrl).netloc)
+    internalLinks = []
+    # 找出所有'/'开头的链接
+    for link in bs.find_all('a', href=re.compile('^(/|.*' + includeUrl + ')')):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                if (link.attrs['href'].startswith('/')):
+                    internalLinks.append(
+                        internalLinks + link.attrs['href']
+                    )
+                else:
+                    internalLinks.append(link.attrs['href'])
+    return internalLinks
+
+
+# 获取页面中所有的外链链接
+def getExternalLinks(bs, excluderUrl):
+    externalLinks = []
+    # 找出所有'http'或'www'开头且不包含当前URL的链接
+    for link in bs.find_all('a', href=re.compile('^(http|www)((?!' + excluderUrl + ').)*$')):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
+
+
+def getRandomExternalLink(startingPage):
+    html = requests.get(startingPage, headers=headers, proxies=proxies, timeout=5)
+    bs = BeautifulSoup(html.text, 'html.parser')
+    externalLinks = getExternalLinks(bs, urlparse(startingPage).netloc)
+    if len(externalLinks) == 0:
+        print('No external links. looking around the site for one')
+        domain = '{}://{}'.format(urlparse((startingPage).scheme, urlparse(startingPage).netloc))
+        internalLinks = getInternalLinks(bs, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks) - 1)])
+    else:
+        return externalLinks[random.randint(0, len(externalLinks) - 1)]
+
+
+def followExternalOnly(startingSite):
+    externalLink = getRandomExternalLink(startingSite)
+    print('Random external link is: {}'.format(externalLink))
+    followExternalOnly(externalLink)
+
+
+followExternalOnly('http://oreilly.com')
