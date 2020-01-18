257d0b5e567bfdbcf1ee840f75646a8c0bfe0c68
KokoroWJY <oumainoli@gmail.com>
2020-01-18 15:14:06 +0800

    爬虫之旅--"Scarpy框架"

--- wikiSpider/wikiSpider/spiders/article.py
+++ wikiSpider/wikiSpider/spiders/article.py
@@ -4,10 +4,13 @@ import scrapy
 ArticleSpider类和文件wikiSpider不一样,
 表明这个类在wikiSpider的众多类目中专门用来抓取文章网页
 """
+
+
 class ArticleSpider(scrapy.Spider):
     name = 'article'
 
     def start_requests(self):
+        # 定义程序的入口, 用于生成Scrapy用来抓取网站的Request对象
         urls = [
             'http://en.wikipedia.org/wiki/Python_'
             '%28programming_language%29'
@@ -17,6 +20,7 @@ class ArticleSpider(scrapy.Spider):
         return [scrapy.Request(url=url, callback=self.parse()) for url in urls]
 
     def parse(self, response):
+        # 是一个用户定义回调函数, 通过callback=self.parse传递给Request对象.
         url = response.url
         title = response.css('h1::text').extract_first()
         print("URL is: {}".format(url))
