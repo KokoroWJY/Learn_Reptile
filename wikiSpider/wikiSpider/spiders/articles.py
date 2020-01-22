2d96ed9fe215220119c75f26187154e5bf391659
KokoroWJY <oumainoli@gmail.com>
2020-01-22 16:20:30 +0800

    爬虫之旅--"Scrapy框架"

--- wikiSpider/wikiSpider/spiders/articles.py
+++ wikiSpider/wikiSpider/spiders/articles.py
@@ -4,6 +4,7 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 class ArticleSpider(CrawlSpider):
     name = 'articles'
     allowed_domains = ['wikipedia.org'] # 保留的域名链接
+
     start_urls = ['http://en.wikipedia.org/wiki/Benevolent_dictator_for_life'] # 开始的链接
     # 正则表达式.*保留所有链接 rules为链接保留提供进一步的说明
     rules = [Rule(LinkExtractor(allow=r'.*'), callback='parse_items', folllow=True)]
