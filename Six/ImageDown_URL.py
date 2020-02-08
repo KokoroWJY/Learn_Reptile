56dc8b41c5fa1a6b73d0c749507b3dd4316702bc
KokoroWJY <oumainoli@gmail.com>
2020-02-08 11:39:01 +0800

    爬虫之旅--"存储数据"

---
+++ Six/ImageDown_URL.py
@@ -0,0 +1,51 @@
+import os
+import requests
+from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+
+proxies = {
+    "http": "http://127.0.0.1:1080",
+    'https': 'http://127.0.0.1:1080'
+}
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
+}
+
+downloadDirectory = 'downloaded'
+baseUrl = 'http://pythonscraping.com'
+
+def getAbsoluteURL(baseUrl, source):
+    if source.startswith('http://www.'):
+        url = 'http://{}'.format(source[11:])
+    elif source.startswith('http://'):
+        url = source
+    elif source.startswith('www.'):
+        url = source[4:]
+        url = 'http://{}'.format(source)
+    else:
+        url = "{}/{}".format(baseUrl, source)
+    if baseUrl not in url:
+        return None
+    return url
+
+def getDownloadPath(baseUrl, absoluteUrl, downloadDirectory):
+    path = absoluteUrl.replace('www.', '')
+    path = path.replace(baseUrl, '')
+    path = downloadDirectory + path
+    directory = os.path.dirname(path)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    return path
+
+html = requests.get('http://www.pythonscraping.com', headers=headers, proxies=proxies)
+bs = BeautifulSoup(html.text, 'html.parser')
+downloadList = bs.findAll(src=True)
+
+for download in downloadList:
+    fileUrl = getAbsoluteURL(baseUrl, download['src'])
+    if fileUrl is not None:
+        print(fileUrl)
+
+urlretrieve(fileUrl, getDownloadPath(baseUrl, fileUrl, downloadDirectory))
